3c847bbbbdf5740fc57a49d78a38701a7dde887f
吴春辉 <wuchunhui@meizu.com>
2017-02-22 19:54:39 +0800

    modify

---
+++ MyOpenGLDemo/app/src/main/java/demo/myopengldemo/mydice/Dice.java
@@ -0,0 +1,185 @@
+package demo.myopengldemo.mydice;
+
+import android.content.Context;
+import android.opengl.GLES20;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
+import demo.myopengldemo.R;
+
+/**
+ * Created by wuchunhui on 17-2-21.
+ */
+
+public class Dice {
+
+    FloatBuffer mVerticesBuffer;//顶点坐标数据缓冲
+    FloatBuffer mNormalBuffer;//顶点法向量数据缓冲
+    FloatBuffer mVerticesTextureBuffer;
+
+    private int mProgram;
+    private Context mContext;
+
+    private int mMVPMatrixHandle;
+    private int mPositionHandle;
+    private int mColorHandle;
+    private int mTextureId;
+    private int mTextureCoordinationHandle;
+
+    public Dice(Context context){
+        mContext = context;
+        initData();
+        initShader();
+    }
+
+    public void initData(){
+
+        // 骰子的顶点数据
+        float[] mDiceVertices = {
+                // Positions
+                -6.0f,  6.0f, -6.0f,//font
+                -6.0f, -6.0f, -6.0f,
+                6.0f, -6.0f, -6.0f,
+                6.0f, -6.0f, -6.0f,
+                6.0f,  6.0f, -6.0f,
+                -6.0f,  6.0f, -6.0f,
+
+                -6.0f, -6.0f,  6.0f,//left
+                -6.0f, -6.0f, -6.0f,
+                -6.0f,  6.0f, -6.0f,
+                -6.0f,  6.0f, -6.0f,
+                -6.0f,  6.0f,  6.0f,
+                -6.0f, -6.0f,  6.0f,
+
+                6.0f, -6.0f, -6.0f,//right
+                6.0f, -6.0f,  6.0f,
+                6.0f,  6.0f,  6.0f,
+                6.0f,  6.0f,  6.0f,
+                6.0f,  6.0f, -6.0f,
+                6.0f, -6.0f, -6.0f,
+
+                -6.0f, -6.0f,  6.0f,//back
+                -6.0f,  6.0f,  6.0f,
+                6.0f,  6.0f,  6.0f,
+                6.0f,  6.0f,  6.0f,
+                6.0f, -6.0f,  6.0f,
+                -6.0f, -6.0f,  6.0f,
+
+                -6.0f,  6.0f, -6.0f,//top
+                6.0f,  6.0f, -6.0f,
+                6.0f,  6.0f,  6.0f,
+                6.0f,  6.0f,  6.0f,
+                -6.0f,  6.0f,  6.0f,
+                -6.0f,  6.0f, -6.0f,
+
+                -6.0f, -6.0f, -6.0f,//bottom
+                -6.0f, -6.0f,  6.0f,
+                6.0f, -6.0f, -6.0f,
+                6.0f, -6.0f, -6.0f,
+                -6.0f, -6.0f,  6.0f,
+                6.0f, -6.0f,  6.0f
+        };
+
+        // 骰子的顶点法线数据
+        float[] mDiceVerticesNormal = {
+                // Positions
+                0.0f,  0.0f, -6.0f,//font
+                0.0f, 0.0f, -6.0f,
+                0.0f, 0.0f, -6.0f,
+                0.0f, 0.0f, -6.0f,
+                0.0f,  0.0f, -6.0f,
+                0.0f,  0.0f, -6.0f,
+
+                -6.0f, 0.0f,  0.0f,//left
+                -6.0f, 0.0f, 0.0f,
+                -6.0f,  0.0f, 0.0f,
+                -6.0f,  0.0f, 0.0f,
+                -6.0f,  0.0f,  0.0f,
+                -6.0f, 0.0f,  0.0f,
+
+                6.0f, 0.0f, 0.0f,//right
+                6.0f, 0.0f,  0.0f,
+                6.0f,  0.0f,  0.0f,
+                6.0f,  0.0f,  0.0f,
+                6.0f,  0.0f, 0.0f,
+                6.0f, 0.0f, 0.0f,
+
+                0.0f, 0.0f,  6.0f,//back
+                0.0f,  0.0f,  6.0f,
+                0.0f,  0.0f,  6.0f,
+                0.0f,  0.0f,  6.0f,
+                0.0f, 0.0f,  6.0f,
+                0.0f, 0.0f,  6.0f,
+
+                0.0f,  6.0f, 0.0f,//top
+                0.0f,  6.0f, 0.0f,
+                0.0f,  6.0f,  0.0f,
+                0.0f,  6.0f,  0.0f,
+                0.0f,  6.0f,  0.0f,
+                0.0f,  6.0f, 0.0f,
+
+                0.0f, -6.0f, 0.0f,//bottom
+                0.0f, -6.0f,  0.0f,
+                0.0f, -6.0f, 0.0f,
+                0.0f, -6.0f, 0.0f,
+                0.0f, -6.0f,  0.0f,
+                0.0f, -6.0f,  0.0f
+        };
+
+        // Initialize the buffers.
+        mVerticesBuffer = ByteBuffer.allocateDirect(mDiceVertices.length * 4)
+                .order(ByteOrder.nativeOrder()).asFloatBuffer();
+        mVerticesBuffer.put(mDiceVertices).position(0);
+
+        mNormalBuffer = ByteBuffer.allocateDirect(mDiceVerticesNormal.length*4)
+                .order(ByteOrder.nativeOrder()).asFloatBuffer();
+        mNormalBuffer.position(0);
+    }
+
+    private void initShader(){
+        mProgram = MyGLUtils.buildProgram(mContext, R.raw.dice_bg_vertex, R.raw.dice_bg_fragment);
+
+        mTextureId = MyGLUtils.loadTexture(mContext,R.drawable.dice,new int[2]);
+
+        // Set program handles. These will later be used to pass in values to the program.
+        mMVPMatrixHandle = GLES20.glGetUniformLocation(mProgram, "u_MVPMatrix");
+//        mTextureUniformHandle = GLES20.glGetUniformLocation(mProgram,"u_Texture");
+        mPositionHandle = GLES20.glGetAttribLocation(mProgram, "a_Position");
+//        mColorHandle = GLES20.glGetAttribLocation(mProgram, "a_Color");
+        mTextureCoordinationHandle = GLES20.glGetAttribLocation(mProgram,"a_TexCoordinate");
+//        mGlobalTimeHandle = GLES20.glGetUniformLocation(mProgram,"u_GlobalTime");
+
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_S, GLES20.GL_REPEAT);
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_T, GLES20.GL_REPEAT);
+    }
+
+    public void drawSelf(float[] mvpMatrix) {
+
+        GLES20.glUseProgram(mProgram);
+
+        // Pass in the position information
+        mVerticesBuffer.position(0);
+        GLES20.glVertexAttribPointer(mPositionHandle, 3, GLES20.GL_FLOAT, false,
+                3*4, mVerticesBuffer);
+        GLES20.glEnableVertexAttribArray(mPositionHandle);
+
+//        mVerticesTextureBuffer.position(0);
+//        GLES20.glVertexAttribPointer(mTextureCoordinationHandle,2, GLES20.GL_FLOAT,false,2*4,mVerticesTextureBuffer);
+//        GLES20.glEnableVertexAttribArray(mTextureCoordinationHandle);
+
+        // This multiplies the view matrix by the model matrix, and stores the result in the MVP matrix
+        // (which currently contains model * view).
+//        Matrix.multiplyMM(mMVPMatrix, 0, mViewMatrix, 0, mModelMatrix, 0);
+        // This multiplies the modelview matrix by the projection matrix, and stores the result in the MVP matrix
+        // (which now contains model * view * projection).
+//        Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mMVPMatrix, 0);
+
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, mTextureId);
+
+        GLES20.glUniformMatrix4fv(mMVPMatrixHandle, 1, false, mvpMatrix, 0);
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 36);
+    }
+
+}
---
+++ MyOpenGLDemo/app/src/main/java/demo/myopengldemo/mydice/DiceSurfaceView.java
@@ -0,0 +1,179 @@
+package demo.myopengldemo.mydice;
+
+import android.content.Context;
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+import android.view.MotionEvent;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+/**
+ * Created by wuchunhui on 17-2-21.
+ */
+
+public class DiceSurfaceView extends GLSurfaceView {
+    private final float TOUCH_SCALE_FACTOR = 180.0f/320;//角度缩放比例
+    private DiceRenderer mRenderer;//场景渲染器
+
+    private float mPreviousY;//上次的触控位置Y坐标
+    private float mPreviousX;//上次的触控位置X坐标
+    //关于摄像机的变量
+    float cx=0;//摄像机x位置
+    float cy=0;//摄像机y位置
+    float cz=-50;//摄像机z位置
+
+    float tx=0;//目标点x位置
+    float ty=0;//目标点y位置
+    float tz=0;//目标点z位置
+    public float currSightDis=60;//摄像机和目标的距离
+    float angdegElevation=0;//仰角
+    public float angdegAzimuth=180;//方位角
+
+    //关于灯光的变量
+    float lx=0;//x位置
+    float ly=0;//y位置
+    float lz=0;//z位置
+    float lightDis=100;
+    float lightElevation=40;//灯光仰角
+    public float lightAzimuth=180;//灯光的方位角
+    public DiceSurfaceView(Context context) {
+        super(context);
+        this.setEGLContextClientVersion(2); //设置使用OPENGL ES2.0
+        mRenderer = new DiceRenderer();	//创建场景渲染器
+        setRenderer(mRenderer);				//设置渲染器
+        setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);//设置渲染模式为主动渲染
+    }
+
+    //触摸事件回调方法
+    @Override
+    public boolean onTouchEvent(MotionEvent e)
+    {
+        float y = e.getY();
+        float x = e.getX();
+        switch (e.getAction()) {
+            case MotionEvent.ACTION_MOVE:
+                float dy = y - mPreviousY;//计算触控笔Y位移
+                float dx = x - mPreviousX;//计算触控笔X位移
+                angdegAzimuth += dx * TOUCH_SCALE_FACTOR;//设置沿x轴旋转角度
+                angdegElevation += dy * TOUCH_SCALE_FACTOR;//设置沿z轴旋转角度
+                //将仰角限制在5～90度范围内
+                angdegElevation = Math.max(angdegElevation, 5);
+                angdegElevation = Math.min(angdegElevation, 90);
+                //设置摄像机的位置
+//                setCameraPostion();
+        }
+        mPreviousY = y;//记录触控笔位置
+        mPreviousX = x;//记录触控笔位置
+        return true;
+    }
+
+    // 位置灯光位置的方法
+    public void setLightPostion() {
+        //计算灯光的位置
+        double angradElevation = Math.toRadians(lightElevation);// 仰角（弧度）
+        double angradAzimuth = Math.toRadians(lightAzimuth);// 方位角
+        lx = (float) (- lightDis * Math.cos(angradElevation)	* Math.sin(angradAzimuth));
+        ly = (float) (+ lightDis * Math.sin(angradElevation));
+        lz = (float) (- lightDis * Math.cos(angradElevation) * Math.cos(angradAzimuth));
+    }
+
+    // 设置摄像机位置的方法
+    public void setCameraPostion() {
+        //计算摄像机的位置
+        double angradElevation = Math.toRadians(angdegElevation);// 仰角（弧度）
+        double angradAzimuth = Math.toRadians(angdegAzimuth);// 方位角
+        cx = (float) (tx - currSightDis * Math.cos(angradElevation)	* Math.sin(angradAzimuth));
+        cy = (float) (ty + currSightDis * Math.sin(angradElevation));
+        cz = (float) (tz - currSightDis * Math.cos(angradElevation) * Math.cos(angradAzimuth));
+    }
+
+    private class DiceRenderer implements GLSurfaceView.Renderer
+    {
+
+        Background mBackground;//地板
+        Dice mDice;//骰子
+
+        public void onDrawFrame(GL10 gl)
+        {
+            //清除深度缓冲与颜色缓冲
+            GLES20.glClear( GLES20.GL_DEPTH_BUFFER_BIT | GLES20.GL_COLOR_BUFFER_BIT);
+            //设置camera位置,在上面往下面看
+            MatrixState.setCamera
+                    (
+                            cx,   //人眼位置的X
+                            cy, 	//人眼位置的Y
+                            cz,   //人眼位置的Z
+                            tx, 	//人眼球看的点X
+                            ty,   //人眼球看的点Y
+                            tz,   //人眼球看的点Z
+                            0, 	//up位置
+                            1,
+                            0
+                    );
+            //初始化光源位置
+            MatrixState.setLightLocation(lx, ly, lz);
+            //若加载的物体部位空则绘制物体
+
+            GLES20.glCullFace(GLES20.GL_FRONT);
+
+            //绘制地板
+            MatrixState.pushMatrix();
+            MatrixState.rotate(-30,1,0,0);
+            mBackground.drawSelf(MatrixState.getFinalMatrix());
+            MatrixState.popMatrix();
+
+            //绘制骰子
+            MatrixState.pushMatrix();
+            MatrixState.translate(0,6,0);
+            MatrixState.rotate(-30,1,1,0);
+            mDice.drawSelf(MatrixState.getFinalMatrix());
+            MatrixState.popMatrix();
+        }
+
+        public void onSurfaceChanged(GL10 gl, int width, int height) {
+            //设置视窗大小及位置
+            GLES20.glViewport(0, 0, width, height);
+            //计算GLSurfaceView的宽高比
+            float ratio = (float) width / height;
+            //调用此方法计算产生透视投影矩阵
+            MatrixState.setProjectFrustum(-ratio, ratio, -1, 1, 2, 100);
+
+            //计算摄像机的位置
+            setCameraPostion();
+            //计算灯光的位置
+            setLightPostion();
+            new Thread(){
+                @Override
+                public void run(){
+                    while(true){
+                        lightAzimuth +=1;
+                        lightAzimuth %= 360;
+                        //改变灯光的位置
+                        setLightPostion();
+                        try {
+                            Thread.sleep(50);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }.start();
+        }
+
+        public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+            //设置屏幕背景色RGBA
+            GLES20.glClearColor(0.3f,0.3f,0.3f,1.0f);
+            //打开深度检测
+            GLES20.glEnable(GLES20.GL_DEPTH_TEST);
+            //关闭背面剪裁
+            GLES20.glDisable(GLES20.GL_CULL_FACE);
+            //初始化变换矩阵
+            MatrixState.setInitStack();
+
+            mBackground = new Background(getContext());
+            mDice = new Dice(getContext());
+        }
+
+    }
+}
